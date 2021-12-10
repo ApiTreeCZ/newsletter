ef10badbd359e2bbf9efbad44b2878448dd41b02
Vít Rozsíval <mail@vitrozsival.cz>
2021-12-10 14:57:13 +0100

    :pencil2: Fix typo in default company name.

--- src/environment.ts
+++ src/environment.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const DRY_RUN = process.env.DRY_RUN === '1';
 export const EMAIL_FROM =
-  process.env.EMAIL_FROM || 'ApiTree s.r.o <info@apitree.cz>';
+  process.env.EMAIL_FROM || 'ApiTree s.r.o. <info@apitree.cz>';
 export const EMAIL_TO = process.env.EMAIL_TO || '';
 export const SEND = process.env.SEND === '1';
 export const SMTP_PASSWORD = process.env.SMTP_PASSWORD || '';
